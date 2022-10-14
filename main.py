dcb0ae7f77316e941aac5508f3f95e840c0bb948
31nkmu <karimovbillal20002@gmail.com>
2022-10-14 19:51:45 +0600

    inicialisation of project

---
+++ main.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import csv
+BASE_URL = 'https://www.mashina.kg/search/all/'
+
+
+def get_html(url):
+    response = requests.get(url)
+    return response.text
+
+
+def get_data(html):
+    soup = bs(html, 'lxml')
+    catalog = soup.find('div', class_='search-results-table')
+    cars = catalog.find_all('div', class_='list-item list-label')
+    for car in cars:
+        try:
+            name = car.find('h2', class_='name').text.strip()
+        except AttributeError:
+            name = ''
+        try:
+            price = car.find('div', class_='block price').text.strip().split('\n')
+            price = [i.strip() for i in price][:-1]
+            price = '\n'.join([i for i in price if i])
+        except AttributeError:
+            price = ''
+        try:
+            images = car.find_all('img', class_='lazy-image')
+            images = '\n'.join([img.get('data-src') for img in images])
+        except AttributeError:
+            images = ''
+        try:
+            info = car.find('div', class_='block info-wrapper item-info-wrapper').text.strip().split('\n')
+            info = [i.strip() for i in info]
+            info = '\n'.join([i for i in info if i])
+        except AttributeError:
+            info = ''
+        write_in_csv({
+            'name': name,
+            'price': price,
+            'images': images,
+            'info': info
+        })
+
+
+def write_in_csv(data):
+    with open('cars.csv', 'a') as file:
+        names = ['name', 'price', 'images', 'info']
+        writer = csv.DictWriter(file, delimiter=',', fieldnames=names)
+        writer.writerow(data)
+
+
+html = get_html(BASE_URL)
+get_data(html)
